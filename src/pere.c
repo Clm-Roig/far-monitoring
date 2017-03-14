c293824e2015a38082e75b76a511f816ac4f5a01
Clément Roig <clm.roig@gmail.com>
2017-03-14 10:37:59 +0100

    Update pere.c

--- src/pere.c
+++ src/pere.c
@@ -7,3 +7,27 @@ const char* NOM_PIPE_PF = "pipePF-";
 const char* NOM_PIPE_FF = "pipeFF-";
 
 // ---- FONCTIONS ---- //
+
+// CREATION
+int creerServeur() {
+    return 1;
+}
+
+int creerPipeW(char* nomPipe) {
+    return 1;
+}
+
+int creerPipeR(char* nomPipe) {
+    return 1;
+}
+
+
+// COMMUNICATION AVEC LES FILS
+
+int pereEnvoiFils(int numFils, char* donnees){
+    return 1;
+}
+
+char* pereReceptionFils(int numFils) {
+    return "toto";
+}   
