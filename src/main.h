fd0146b325a7cfb9437d26b606806eb9b44d5828
Clément Roig <clm.roig@gmail.com>
2017-03-14 09:42:38 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -2,11 +2,18 @@
 #define DEF_MAIN
 
 // EN-TETE main.h
+
+// Sockets / HTTP / IP
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+// Threads 
+#include <pthread.h>
+
+// Base 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
