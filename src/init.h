f187b0707d27a98e334d7e12d97a2ec897e36a16
Hlecler <Hugo.lecler@laposte.net>
2017-03-14 09:57:03 +0100

    Create init.h

---
+++ src/init.h
@@ -0,0 +1,33 @@
+#ifndef DEF_MAIN
+#define DEF_MAIN
+
+// EN-TETE main.h
+
+// Sockets / HTTP / IP
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Threads 
+#include <pthread.h>
+
+// Base 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// ---- CONSTANTES ---- //
+
+
+// ---- FONCTIONS ---- //
+
+/**
+  @brief Obtient les identifiants des robots puis intiailise le père et lui fournit les informations
+  @return int, 1 si tout s'est bien passé, 0 sinon
+*/
+int init();
+
+#endif
