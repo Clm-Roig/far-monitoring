a3e78dd07891ff001d02ab1afff7de6a3e992219
Clément Roig <clm.roig@gmail.com>
2017-03-14 10:54:24 +0100

    Update serveur.h

--- src/serveur.h
+++ src/serveur.h
@@ -1,1 +1,33 @@
+#ifndef DEF_SERVEUR
+#define DEF_SERVEUR
 
+// EN-TETE serveur.h
+
+// Sockets / HTTP / IP
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Threads 
+#include <pthread.h>
+
+// Base 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// ---- CONSTANTES ---- //
+
+
+// ---- FONCTIONS ---- //
+
+/**
+  @brief Initialisation du serveur
+  @return int, 1 si tout s'est bien passé, 0 sinon
+*/
+int init();
+
+#endif
