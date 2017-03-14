f4af49744bfbdd041e2050226f155743bad6e596
Clément Roig <clm.roig@gmail.com>
2017-03-14 09:39:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// Fichier main.c
 
+#include "main.h"
+
+// ---- CONSTANTES ---- //
+
+// ---- FONCTIONS ---- //
