eced882fde231f7962419ee934fc018bbe0f2518
Hlecler <Hugo.lecler@laposte.net>
2017-03-14 10:58:00 +0100

    Create fils.c

---
+++ src/fils.c
@@ -0,0 +1,47 @@
+// Fichier fils.c
+
+#include "fils.h"
+
+// ---- CONSTANTES ---- //
+
+const int LONGUEUR_GRILLE = 10; // TODO
+const int LARGEUR_GRILLE = 10; // TODO
+
+// ---- FONCTIONS ---- //
+
+int creerFils(); 
+{
+
+
+    return 1;
+}
+
+// ---- COMMUNICATION PERE ---- //
+
+int filsEnvoiPere(char* nomPipe, char* donnees);
+{
+
+}
+
+char* filsReceptionPere(char* nomPipe);
+{
+
+}
+
+int* saisirCoord();
+{
+
+}
+
+
+// ---- COMMUNICATION FILS ---- //
+
+int filsEnvoiFils(char* nomPipe, char* donnees);
+{
+
+}
+
+char* filsReceptionFils(char* nomPipe)
+{
+
+}
