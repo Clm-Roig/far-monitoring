c0c88935ee559aea478b501007152d31d15d52fd
Clément Roig <clm.roig@gmail.com>
2017-03-14 09:38:31 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,1 +1,21 @@
+#ifndef DEF_MAIN
+#define DEF_MAIN
 
+// EN-TETE main.h
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// ---- CONSTANTES ---- //
+
+
+// ---- FONCTIONS ---- //
+
+
+#endif
