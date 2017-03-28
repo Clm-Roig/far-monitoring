9d825ecfdd759b33ad71573b03dcc727e9ab3bf2
Hlecler <Hugo.lecler@laposte.net>
2017-03-28 14:21:20 +0200

    Update util.c

--- src/util.c
+++ src/util.c
@@ -1,1 +1,15 @@
+// Fichier util.c
+
+#include "util.h"
+
+// ---- CONSTANTES ---- //
+
+// ---- FONCTIONS ---- //
+
+int initSocket(int port) {
+    return 1;
+}
+
+int sendtoSocket(int socket, char* data) {
+    return 1;    
 
