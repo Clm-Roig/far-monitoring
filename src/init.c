d105fd7bbc5c5c919a4c95d2653e2ef8a4d0492c
Hlecler <Hugo.lecler@laposte.net>
2017-03-28 14:43:16 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -3,10 +3,17 @@
 #include "init.h"
 
 // ---- CONSTANTES ---- //
+const int PORT = 2000;
 
 // ---- FONCTIONS ---- //
+int errno = 0;
 
- int init() {
-   
-   return 1;
+int init() {
+    //int verif = initSocket(PORT);
+    int nul = 5/0;
+    if (errno != 0) 
+    {
+        fprintf("Erreur d'initialisation de socket: %s.\n", strerror(errno));
+    }
+    return 1;
 }
