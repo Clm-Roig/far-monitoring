8fddd53384445148d185f06914f93ad1dc7dbc1f
Hlecler <Hugo.lecler@laposte.net>
2017-04-24 14:53:42 +0200

    Update pere.c

--- src/pere.c
+++ src/pere.c
@@ -4,13 +4,22 @@
 
 // ---- CONSTANTES ---- //
 const char* NOM_PIPE_PF = "pipePF-"; // TODO : inutile pour le moment, à voir comment l'implémenter
-const char* NOM_PIPE_FF = "pipeFF-"; // TODO : inutile pour le moment, à voir comment l'implémenter
-
+const char* NOM_PIPE_FF = "pipeFF-"; // TODO : inutile pour le moment, à voir comment l'implémenter 
 // ---- FONCTIONS ---- //
+int* tableauPID [6];
 
 // CREATION
 int creerPere(char** tabIPs) {
-    return 1;
+    
+    for (int i = 0; i <6; i++)    
+    {
+        tableauPID[i] = fork();
+        
+    }
+}
+
+char* genererToken() {
+    return "toto";
 }
 
 // COMMUNICATION AVEC LES FILS
@@ -18,6 +27,14 @@ int pereEnvoiFils(int numFils, char* donnees){
     return 1;
 }
 
-char* pereReceptionFils(int numFils) {
+char* pereReceptionFilsChar(int numFils) {
     return "toto";
 }
+
+int pereReceptionFilsInt(int numFils) {
+    return 1;    
+}
+
+//COMMUNICATION AVEC LE SERVEUR
+
+int pereEnvoie
