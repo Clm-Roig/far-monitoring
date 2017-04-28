73fed0f9793131be296d0788bf3f3b10ce4898d8
Hlecler <Hugo.lecler@laposte.net>
2017-04-28 08:53:12 +0200

    Update serveur.h

--- src/serveur.h
+++ src/serveur.h
@@ -31,9 +31,25 @@
 
 /**
   @brief Initialisation du serveur
-  @param Le tableau des adresses IP des robots
+  @param int AddrIp, Le tableau des adresses IP des robots
   @return int, 1 si tout s'est bien passé, 0 sinon
 */
 int creerServeur(char** AddrIp);
+
+/**
+  @brief Envoi des logs de coordonnées sur Dweet.io
+  @param Coordonnees, Les coordonnées à envoyer sous forme de log
+  @return int, 1 si tout s'est bien passé, 0 sinon
+  
+*/
+int envoiDweet(int* Coordonnees);
+
+/**
+  @brief Envoi des coordonnées aux robots
+  @param int* Coordonnees, Les coordonnées à envoyer au robot
+  @param char* AddrRobot, l'adresse IP du robot auquel on envoie les coordonnées
+  @return int, 1 si tout s'est bien passé, 0 sinon
+*/
+int envoiRobot(int* Coordonnees, char* AddrRobot);
 
 #endif
