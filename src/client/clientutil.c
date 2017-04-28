60b9fd13286b8b5d0023968a49945a95057c0010
Hlecler <Hugo.lecler@laposte.net>
2017-04-28 14:36:24 +0200

    Update clientutil.c

--- src/client/clientutil.c
+++ src/client/clientutil.c
@@ -1,1 +1,21 @@
+ // Fichier clientutil.c
+ 
+ #include "clientutil.h"
 
+  // ---- FONCTIONS ---- //
+
+int initSocket(int port, char* IP) {
+    return 0;
+}
+
+ int receiveFromSocket(int socket, char** data) {
+     recv(socket,&data,sizeof(&data)-1,0);
+     // Erreur ?
+     if(errno) {
+         printf("\nErreur reception données socket (receiveFromSocket() dans utilclient.c) par le socket %d", socket);
+         perror("");
+         exit(-1);
+     }
+     return 0;
+ }
+ 
