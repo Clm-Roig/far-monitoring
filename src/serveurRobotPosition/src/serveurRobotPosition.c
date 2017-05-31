056cab67d52c15b17443ea10d931218ffac2b4d0
Clément Roig <clm.roig@gmail.com>
2017-05-31 11:23:54 +0200

    [servRobot] misc

--- src/serveurRobotPosition/src/serveurRobotPosition.c
+++ src/serveurRobotPosition/src/serveurRobotPosition.c
@@ -13,7 +13,12 @@ char* divers;
 // ---- FONCTIONS ---- //
 
 int main() {
+	// Récupération de l'id du SP
+
+	
+
 	int sockServeurPos = initSocket(80,ipServeurPosition,"null");
+
 	char* data[3];
 	data[0] = malloc(32*sizeof(char));
 	data[1] = malloc(32*sizeof(char));
@@ -21,11 +26,14 @@ int main() {
 
 	divers = malloc(256*sizeof(char));
 	while(!checkFinPartie()) {
-		// attente des coordonnées sur le socket
+		// Attente des coordonnées sur le socket
 		receiveFromSocket(sockServeurPos, data);
 
 		x = atoi(data[0]);
 		y = atoi(data[1]);
 		divers = data[2];
 	}
+	// fin de partie
+
+	return 0;
 }
