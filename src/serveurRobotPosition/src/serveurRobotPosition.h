95fc30b9e690098e0ca4fc26cbff8d4dc1f36058
Clément Roig <clm.roig@gmail.com>
2017-05-31 14:40:13 +0200

    [servRobot] plus include

--- src/serveurRobotPosition/src/serveurRobotPosition.h
+++ src/serveurRobotPosition/src/serveurRobotPosition.h
@@ -32,4 +32,38 @@
 */
 int lancerServeurPosition();
 
+// ---- FONCTIONS ---- //
+
+// Sockets
+/**
+    @brief crée + connecte un socket sur le port demandé (protocole IP) par IP ou URL (l'un doit être  initialisé à "null" pour pouvoir utiliser l'autre)
+    @param int port : le port sur lequel l'on doit se connecter
+    @param char* IP : l'ip sur laquelle on veut se connecter
+    @param char* URL : l'url sur laquelle on souhaite se connecter
+    @return int, le socket créé et connecté, -1 si la création échoue
+*/
+int initSocket(int port, char* IP, char* URL);
+
+
+/**
+    @brief reçoit des données par un socket
+    @param int socket : le socket sur lequel on reçoit les données
+    @param char** buffer : l'adresse du buffer à remplir
+    @return bool, 1 si ok, -1 sinon
+*/
+int receiveFromSocket(int socket, char** buffer);
+
+/**
+    @brief Vérifie si la partie est terminé
+    @return int, 1 si la partie est terminée, 0 sinon
+*/
+int checkFinPartie();
+
+/**
+    @brief Renvoie les données contenues dans le dernier message sur Beebotte du type spécifié
+    @param char* typedonnee, le type de messages que l'on souhaite récupérer (partie "type_msg" sur Beebotte)
+    @return char* les données contenues dans la partie "data" du msg
+*/
+char* recepBeebotte(char* typedonnee);
+
 #endif
