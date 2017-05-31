92c18269abc4ad73a8f847b94c62408ff28a6705
Hlecler <Hugo.lecler@laposte.net>
2017-05-31 12:16:02 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -228,15 +228,70 @@ char* recepBeebotte(char* typedonnee)
         char *host = "api.beebotte.com";
 
     char path[200] = "/v1/public/data/read/vberry/";
-    strcat(path,canal); strcat(path,"/"); strcat(path,ressource);
+    strcat(path,canal); strcat(path,"/"); strcat(path,ressource2);
 
     struct hostent *server;
     struct sockaddr_in serv_addr;
     int sockfd, bytes, sent, received, total, message_size;
-    char *message, response[4096];
+    char response[4096];
     
+    sprintf(message,"GET %s HTTP/1.0\r\n",path);      
 
+    // Socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        Error("ERROR opening socket");
+    }
+
+    // Host
+    server = gethostbyname(host);
+    if (server == NULL) {
+        Error("ERROR, no such host");
+    }
+
+    /* fill in the structure (port 80)*/
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(80); 
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+        Error("ERROR connecting");
+    }
+
+    /* receive the response */
     
+    total = strlen(message);
+    sent = 0;
+    do {
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            Error("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent+=bytes;
+    } while (sent < total);
+    
+        memset(response,0,sizeof(response));
+    total = sizeof(response)-1;
+    received = 0;
+    do {
+        bytes = read(sockfd,response+received,total-received);
+        if (bytes < 0) Error("ERROR reading response from socket");
+        if (bytes == 0)
+            break;
+        received+=bytes;
+    } while (received < total);
+
+    if (received == total) {
+        Error("ERROR storing complete response from socket");
+    }
+    close(sockfd);
+    
+    /* Traitement du message reçu*/
+    
+    printf(%s, response);
     return "";
 }
 int envoiRobot(char* donnees, char* addrRobot){
