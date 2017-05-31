ea00552acaed2cf035acd9981aa453323ab5a9c7
Clément Roig <clm.roig@gmail.com>
2017-05-31 11:50:09 +0200

    [main] préparation du main de test de la réception Beebotte

--- src/main.c
+++ src/main.c
@@ -7,13 +7,13 @@
 // ---- FONCTIONS ---- //
 
 int main(int argc,char* args[]) {
-
+/*
 	// Pour créer un serveur en local sur le terminal :
 	// nc -l localhost "numDuPort"
 	// nc -l localhost 1234
 
 	// Test socket
-/*
+
 	char* tableauIPs[6];
 
 	tableauIPs[0] = "128.42.42.32";
@@ -24,8 +24,16 @@ int main(int argc,char* args[]) {
 	tableauIPs[5] = "133.42.42.32";
 	creerFils(tableauIPs);
 */
-	char* data [4] = {"3","SP","1","1,1,123.5.5.5"};
+
+	// Test envoi Beebotte
+	char* data [4] = {"COORD","SP","1","1,1,123.5.5.5"};
 	envoiBeebotte(data);
+
+	// Test réception Beebotte
+	char* donnees = malloc(512*sizeof(char));
+	donnees = recepBeebotte("COORD");
+
+
 
     return 0;
 }
